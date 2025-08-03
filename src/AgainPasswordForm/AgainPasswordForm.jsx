4c699aa5e8e72145df0eed3878c86d4e8e2600c0
Darina <darinakrasilnikova1@gmail.com>
2025-08-03 18:13:48 +0300

    deleted some code

--- src/AgainPasswordForm/AgainPasswordForm.jsx
+++ src/AgainPasswordForm/AgainPasswordForm.jsx
@@ -1,36 +1,6 @@
 import styles from "../App/app.module.css";
 import { useState } from "react";
 
-function AgainPasswordFormLayout({ userData, errorValidate }) {
-  const onAgainPasswordChange = ({ target }) => {
-    userData.againPassword.setUserData(target.value);
-    if (target.value !== userData.password && target.value !== "") {
-      userData.againPasswordError.setErrorValidate("Пароли не совпадают");
-    } else if (
-      target.value === userData.password ||
-      target.value === "" ||
-      userData.password === ""
-    ) {
-      userData.againPasswordError.setErrorValidate("");
-    }
-  };
-
-  return (
-    <>
-      <input
-        className={styles.form}
-        type="text"
-        name="againPassword"
-        placeholder="Еще раз пароль"
-        value={userData.againPassword}
-        onChange={onAgainPasswordChange}
-      />
-
-      <div className={styles.error}>{errorValidate.againPasswordError}</div>
-    </>
-  );
-}
-
 export default function AgainPasswordForm({ name, label, error, ...props }) {
   return (
     <>
